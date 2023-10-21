5b6f78b159344c0e47f92bb00f6d642c2c1b8f5f
Grin777777 <116489301+Grin777777@users.noreply.github.com>
2023-10-21 17:32:39 +0300

    laba1.sql

--- laba1.sql
+++ laba1.sql
@@ -1,57 +1,42 @@
---
--- File generated with SQLiteStudio v3.1.0 on ѕн окт 9 16:24:11 2023
---
--- Text encoding used: System
---
-PRAGMA foreign_keys = off;
-BEGIN TRANSACTION;
-
--- Table: SportingEvents
-CREATE TABLE SportingEvents (
-    EventID INTEGER PRIMARY KEY AUTOINCREMENT,
-    EventName TEXT NOT NULL,
-    StartDate DATE NOT NULL,
-    EndDate DATE,
-    Location TEXT
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  surname VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL UNIQUE,
+  password VARCHAR(50) NOT NULL
 );
 
--- Table: Teams
-CREATE TABLE Teams (
-    TeamID INTEGER PRIMARY KEY AUTOINCREMENT,
-    TeamName TEXT NOT NULL,
-    CoachName TEXT,
-    TeamLocation TEXT
+CREATE TABLE subjects (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL
 );
 
--- Table: Results
-CREATE TABLE Results (
-    ResultID INTEGER PRIMARY KEY AUTOINCREMENT,
-    EventID INTEGER NOT NULL,
-    DisciplineID INTEGER NOT NULL,
-    ParticipantID INTEGER NOT NULL,
-    ResultTime REAL,
-    FOREIGN KEY (EventID) REFERENCES SportingEvents (EventID),
-    FOREIGN KEY (DisciplineID) REFERENCES Disciplines (DisciplineID),
-    FOREIGN KEY (ParticipantID) REFERENCES Participants (ParticipantID)
+CREATE TABLE topics (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  subject_id SERIAL NOT NULL REFERENCES subjects(id)
 );
 
--- Table: Disciplines
-CREATE TABLE Disciplines (
-    DisciplineID INTEGER PRIMARY KEY AUTOINCREMENT,
-    DisciplineName TEXT NOT NULL,
-    Description TEXT
+CREATE TABLE materials (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT,
+  topic_id INTEGER NOT NULL REFERENCES topics(id)
 );
 
--- Table: Participants
-CREATE TABLE Participants (
-    ParticipantID INTEGER PRIMARY KEY AUTOINCREMENT,
-    FirstName TEXT NOT NULL,
-    LastName TEXT NOT NULL,
-    DateOfBirth DATE NOT NULL,
-    Gender TEXT,
-    TeamID INTEGER,
-    FOREIGN KEY (TeamID) REFERENCES Teams (TeamID)
+CREATE TABLE tasks (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT,
+  material_id INTEGER NOT NULL REFERENCES materials(id),
+  created_at TIMESTAMP DEFAULT NOW(),
+  updated_at TIMESTAMP DEFAULT NOW()
 );
 
-COMMIT TRANSACTION;
-PRAGMA foreign_keys = on;
+CREATE TABLE results (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER NOT NULL REFERENCES users(id),
+  task_id INTEGER NOT NULL REFERENCES tasks(id),
+  mark INTEGER NOT NULL,
+  completed_at TIMESTAMP DEFAULT NOW()
+);
